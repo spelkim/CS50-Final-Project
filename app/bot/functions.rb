09879a3c9ff2e17c79a0025d532135117797af1e
Spencer Kim <spelkim@gmail.com>
2016-12-01 01:44:00 -0500

    weather v1

--- app/bot/functions.rb
+++ app/bot/functions.rb
@@ -1,4 +1,4 @@
-require 'open-uri'
+#require 'open-uri'
 
-weather = open('api.openweathermap.org/data/2.5/weather?zip=#{user_zipcode},us')
+#weather = open('api.openweathermap.org/data/2.5/weather?zip=#{user_zipcode},us')
 
