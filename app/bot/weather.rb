b8a9267fc229c158a7f8843bc48b54a658218449
Spencer Kim <spelkim@gmail.com>
2016-12-01 20:34:12 -0500

    weather API v3

--- app/bot/weather.rb
+++ app/bot/weather.rb
@@ -36,7 +36,7 @@ Bot.on :message do |message|
   	response = Net::HTTP.get(uri)
   	weather = JSON.parse(response)
   	# make clothing recommendation
-  	message.reply(text: "#{weather[name]}")
+  	message.reply(text: "#{weather.cod}")
   else
   	message.reply(text: 'No idea what you\'re saying')
   end
