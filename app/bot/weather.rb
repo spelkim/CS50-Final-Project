481acf8908dc405cb95f34cca63a47f662a3bae9
Spencer Kim <spelkim@gmail.com>
2016-12-01 20:08:45 -0500

    weather API v1

--- app/bot/weather.rb
+++ app/bot/weather.rb
@@ -31,7 +31,7 @@ Bot.on :message do |message|
   	end
   when /wear/i
   	# access weather API
-  	weather = open('http://api.openweathermap.org/data/2.5/weather?zip=#{user_zipcode},us&appid=60a63f39a6b259fc6aa363e5f0879ddf')
+  	weather = open('http://api.openweathermap.org/data/2.5/weather?zip=02138,us&appid=60a63f39a6b259fc6aa363e5f0879ddf')
   	# make clothing recommendation
   	message.reply(text: "#{weather.name}")
   else
