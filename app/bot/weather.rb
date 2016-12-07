1a85768e65995ef75a377cbee8b3ce44bcbc54f2
Spencer Kim <spelkim@gmail.com>
2016-12-07 14:10:24 -0500

    fixed typo

--- app/bot/weather.rb
+++ app/bot/weather.rb
@@ -7,12 +7,12 @@ include Facebook::Messenger
 
 # if bot receives message
 Bot.on :message do |message|
-  message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
-  message.sender      # => { 'id' => '1008372609250235' }
-  message.seq         # => 73
-  message.sent_at     # => 2016-04-22 21:30:36 +0200
-  message.text        # => 'Hello, bot!'
-  message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
+  # message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
+  # message.sender      # => { 'id' => '1008372609250235' }
+  # message.seq         # => 73
+  # message.sent_at     # => 2016-04-22 21:30:36 +0200
+  # message.text        # => 'Hello, bot!'
+  # message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
 
   # if bot receives a message with text
   case message.text
@@ -152,6 +152,6 @@ Bot.on :postback do |postback|
 		# reply to user with instructions for use
   		postback.reply(text: "Welcome to Weather the Weather! We'll give you clothing recommendations based on the weather in your current location. Start by sending us your zipcode in the format: 'zipcode *your zipcode*' (ex: zipcode 12345).")
   		postback.reply(text: "After updating your zipcode, you can type 'clothes' for clothing recommendations, 'weather' for current weather, or update your personal preference so that we can give you better recommendations based on your typical warmth or coldness relative to others.")
-  		postback.reply(text: "For instructions on updating your personal preference or using any other part of the bot, message 'help' for usage instrutions.")
+  		postback.reply(text: "For instructions on updating your personal preference or using any other part of the bot, message 'help' for usage instructions.")
   	end
 end
