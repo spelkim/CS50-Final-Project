6d10de500001a1f4e622b5ccf93c4475fc0f5a1d
Spencer Kim <spelkim@gmail.com>
2016-12-07 16:45:59 -0500

    hi

--- app/bot/weather.rb
+++ app/bot/weather.rb
@@ -7,18 +7,12 @@ include Facebook::Messenger
 
 # if bot receives message
 Bot.on :message do |message|
-  # message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
-  # message.sender      # => { 'id' => '1008372609250235' }
-  # message.seq         # => 73
-  # message.sent_at     # => 2016-04-22 21:30:36 +0200
-  # message.text        # => 'Hello, bot!'
-  # message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
 
   # if bot receives a message with text
   case message.text
   
   # when message includes the given word, in this case "hello"  	
-  when /hello/i
+  when /hello/i or /hi/i
   	message.reply(text: 'Hello, human!')
   
   # provide user with usage instructions
